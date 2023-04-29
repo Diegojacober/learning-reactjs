facc6adb1f5a93b2e222929b86bf42e202f540e0
Diego <diegoalencar.jacober@gmail.ocm>
2023-04-29 12:21:01 -0300

    Componentes e props pt2

--- curso01/modulo01/src/App.js
+++ curso01/modulo01/src/App.js
@@ -9,14 +9,49 @@ const Bemvindo = (props) => {
   )
 }
 
+const Equipe = (props) => {
+  return(
+    <div>
+     <Sobre nome={props.nome} 
+     cargo={props.cargo}
+      idade={props.idade}/>
+      <Social github={props.github}/>
+      <hr/>
+    </div>
+  )
+}
+
+const Sobre = (props) => {
+  return(
+    <div>
+      <h2>Olá meu nome é {props.nome}</h2>
+      <small>Sou {props.cargo}</small>
+    </div>
+  )
+}
+
+const Social = (props) => {
+  return(
+    <div>
+      <ul>
+        <li><a href={props.github}>Github</a></li>
+      </ul>
+    </div>
+  )
+}
+
 // Criando um componente chamado app
 export default function App() {
   return (
     <div>
       {/* //chamando um componente e passando as propriedades */}
-      <Bemvindo nome="Diego Alencar" idade="18"/>
+      {/* <Bemvindo nome="Diego Alencar" idade="18"/>
       
-      <h2>Diego Jacober</h2>
+      <h2>Diego Jacober</h2> */}
+
+
+      <h1>Conheça nossa equipe</h1>
+      <Equipe nome="Diego" cargo="Programador" idade="18" github="https://github.com/diegojacober"/>
     </div>
   )
 }
