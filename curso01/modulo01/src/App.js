f68a8d3fa39a5d7573c45c0ae8069dc977fe0545
Diego <diegoalencar.jacober@gmail.ocm>
2023-04-29 12:01:40 -0300

    Componentes e props

--- curso01/modulo01/src/App.js
+++ curso01/modulo01/src/App.js
@@ -1,9 +1,21 @@
 
+//stateless comoponente
+const Bemvindo = (props) => {
+  return(
+    <div>
+      <h4>Bem vindo(a) {props.nome}</h4>
+      <small>Você tem {props.idade} anos</small>
+    </div>
+  )
+}
+
 // Criando um componente chamado app
 export default function App() {
   return (
     <div>
-      <h1>Bem vindo ao sistema</h1>
+      {/* //chamando um componente e passando as propriedades */}
+      <Bemvindo nome="Diego Alencar" idade="18"/>
+      
       <h2>Diego Jacober</h2>
     </div>
   )
