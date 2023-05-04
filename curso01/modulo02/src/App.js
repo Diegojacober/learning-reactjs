445decf6aadb142ec9a54bb6d23922d0d98d6a1e
Diegojacober <diegoalencar.jacober@gmail.com>
2023-05-04 16:02:19 -0300

    Formularios

--- curso01/modulo02/src/App.js
+++ curso01/modulo02/src/App.js
@@ -45,11 +45,16 @@ class App extends Component{
         {id: 1, username: "Diego", curtidas:10, comentarios:2},
         {id: 2, username: "Ângelo", curtidas:5, comentarios:4},
         {id: 2, username: "Cadu", curtidas:0, comentarios:4}
-      ]
+      ],
+      email: '',
+      password: '',
+      sexo: ''
     }
 
     this.entrar = this.entrar.bind(this)
     this.sair = this.sair.bind(this)
+    this.changeEmail = this.changeEmail.bind(this)
+    this.changeSexo = this.changeSexo.bind(this)
   }
 
   entrar() {
@@ -97,20 +102,58 @@ class App extends Component{
   // }
 
   //trabalhando com listas
+  // render() {
+  //   return(
+  //     <div>
+
+  //   {this.state.feed.map((item) => {
+  //           return(
+  //             <Post username={item.username} 
+  //             id={item.id}
+  //             comentarios={item.comentarios} 
+  //             curtidas={item.curtidas}/>
+  //           )})
+  //         }
+
+  //   </div>
+  //   )
+  // }
+
+  changeEmail(e) {
+    let valorDigitado = e.target.value
+    this.setState({email: valorDigitado})
+  }
+
+  changeSexo(e) {
+    this.setState({sexo: e.target.value})
+  }
+
+  //formulário
   render() {
     return(
+      <>
+        <h2>Login</h2>
+        <label for="email">Email:</label>
+        <input type="email" name="email" value={this.state.email}
+          onChange={this.changeEmail}/>
+        <br/>
+        <label for="password">Password:</label>
+        <input type="password" name="password" value={this.state.password}
+        onChange={(e) => {this.setState({password: e.target.value})}}/>
+        <br/>
+        Sexo:
+        <select name="sexo" value={this.state.sexo} onChange={this.changeSexo}>
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+        </select>
       <div>
+        <h3>{this.state.email}</h3>
+        <h3>{this.state.password}</h3>
+        <h3>{this.state.sexo}</h3>
+      </div>
+      </>
 
-    {this.state.feed.map((item) => {
-            return(
-              <Post username={item.username} 
-              id={item.id}
-              comentarios={item.comentarios} 
-              curtidas={item.curtidas}/>
-            )})
-          }
-
-    </div>
+      
     )
   }
 }
