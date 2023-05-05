3a687a2ca25083cd413609f9a32e368f28a0cd6d
Diegojacober <diegoalencar.jacober@gmail.com>
2023-05-05 16:29:59 -0300

    useCallback

--- curso01/hooks/usecallback/src/App.js
+++ curso01/hooks/usecallback/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useMemo, useCallback} from 'react';
 
 function App() {
+
+  //primeiro parametro -> nome da state. segundo parametro -> função de atualização do state
+  const [tarefas, setTarefas] = useState([
+])
+
+  const [nome, Setnome] = useState('Diego')
+
+  const [input, setInput] = useState('')
+
+  // toda vez que essa state(tarefas) for alterada ele chama essa função
+  //substituir o component didUptade
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+  }, [tarefas])
+
+  //substituir o component didMount
+
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('tarefas')
+
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage))
+    }
+    
+  }, [])
+
+  //adicionar tarefa
+  // utilizando o callback poupa processamento pois não é necessário recriar na memória
+  const handleAdd = useCallback(() => {
+    setTarefas([...tarefas, input])
+    setInput('')
+  }, [tarefas, input])
+
+  //toda vez que alterar uma tarefa, chamar essa função, utilizado para cálculos
+  const totalTarefas = useMemo(() => { 
+    return tarefas.length > 0 ? tarefas.length : 0
+   }, [tarefas])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hooks</h1>
+      {nome}
+      <div>
+
+        <ul>
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>{tarefa}</li>
+          ))}
+        </ul>
+        <hr/>
+        <strong>Você tem {totalTarefas} tarefas!</strong>
+      </div>
+
+      <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
+      <button type='button' onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
