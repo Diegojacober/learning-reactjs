585c6b4bb7aa4847b109024f8b0c5ae6437a6ac9
Diegojacober <diegoalencar.jacober@gmail.com>
2023-05-05 14:10:41 -0300

    Use effect

--- curso01/hooks/useeffect/src/App.js
+++ curso01/hooks/useeffect/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  //primeiro parametro -> nome da state. segundo parametro -> função de atualização do state
+  const [tarefas, setTarefas] = useState([
+])
+
+  const [nome, Setnome] = useState('Diego')
+
+  const [input, setInput] = useState('')
+
+  // toda vez que essa state(tarefas) for alterada ele chama essa função
+  //substituir o component didUptade
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+  }, [tarefas])
+
+  //substituir o component didMount
+
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('tarefas')
+
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage))
+    }
+    
+  }, [])
+
+  function handleAdd() {
+    setTarefas([...tarefas, input])
+    setInput('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hooks</h1>
+      {nome}
+      <div>
+
+        <ul>
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>{tarefa}</li>
+          ))}
+        </ul>
+      </div>
+
+      <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
+      <button type='button' onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
