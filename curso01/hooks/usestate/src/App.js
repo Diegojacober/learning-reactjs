a406c440b217fb81c25fa93741fefa15e3e7fc6f
Diegojacober <diegoalencar.jacober@gmail.com>
2023-05-05 13:56:58 -0300

    Use state hooks

--- curso01/hooks/usestate/src/App.js
+++ curso01/hooks/usestate/src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  //primeiro parametro -> nome da state. segundo parametro -> função de atualização do state
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar react hooks'
+  ])
+
+  const [nome, Setnome] = useState('Diego')
+
+  const [input, setInput] = useState('')
+
+  function handleAdd() {
+    setTarefas([...tarefas, input])
+    setInput('')
+  }
+
   return (
     <div className="App">
-      
+      <h1>Hooks</h1>
+      {nome}
+      <div>
+
+        <ul>
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>{tarefa}</li>
+          ))}
+        </ul>
+      </div>
+
+      <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
+      <button type='button' onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
