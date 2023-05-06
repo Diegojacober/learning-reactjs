fff10d139243fd69daf943653576d9e031fbaff1
Diego <diegoalencar.jacober@gmail.ocm>
2023-05-06 16:22:55 -0300

    Criando o header

--- curso01/projeto-filmes/src/routes.js
+++ curso01/projeto-filmes/src/routes.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Filme from './pages/Filme'
 import Erro from "./pages/Erro";
+import Header from "./components/Header";
 
 function RoutesApp() {
     return(
         <BrowserRouter>
+           <Header/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/filme/:id" element={<Filme/>}/>
@@ -18,4 +20,4 @@ function RoutesApp() {
     )
 }
 
-export default RoutesApp
+export default RoutesApp 
