105d67db4947fc8fa7a7a6d76e609829a6eddc8f
Diego <diegoalencar.jacober@gmail.ocm>
2023-05-06 14:46:37 -0300

    Configurando as rotas

---
+++ curso01/projeto-filmes/src/pages/Erro/index.js
@@ -0,0 +1,11 @@
+function Erro() {
+    return(
+        <div>
+        
+            <h1>404 - Verifique o link, você caiu em uma página não encontrada</h1>
+
+        </div>
+    )
+}
+
+export default Erro
---
+++ curso01/projeto-filmes/src/pages/Filme/index.js
@@ -0,0 +1,9 @@
+function Filme() {
+    return(
+        <>
+        <h1>Bem vindo a pagina detalhes de filme</h1>
+        </>
+    )
+}
+
+export default Filme
