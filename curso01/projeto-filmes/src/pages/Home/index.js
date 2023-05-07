5d31e0bc8b1f5d028c12383367a7a79c5016733e
Diego <diegoalencar.jacober@gmail.ocm>
2023-05-07 09:02:15 -0300

    criando a home

--- curso01/projeto-filmes/src/pages/Home/index.js
+++ curso01/projeto-filmes/src/pages/Home/index.js
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react"
+import api from "../../services/api"
+import { Link } from "react-router-dom"
+import './home.css'
+
+// movie/now_playing?api_key=b61329eeeafc2d005a464e4a33bddb30
+
 function Home() {
+
+    const [filmes, setFilmes] = useState([])
+
+    // roda quando a aplicação inicia
+    useEffect(() => {
+
+        async function loadFilmes() {
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "b61329eeeafc2d005a464e4a33bddb30",
+                    language: "pt-BR",
+                    page: 1
+                }
+            })
+
+            //pega os 10 primeiros filmes
+            setFilmes(response.data.results.slice(0,10))
+        }
+
+        loadFilmes()
+    }, [])
+
     return(
-        <>
-        <h1>Bem vindo a home</h1>
-        </>
+        <div className="container">
+            <div className="filmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
