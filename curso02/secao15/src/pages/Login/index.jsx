b520948046c0f711c2d71687df78c23b895b9176
Diego <diegoalencar.jacober@gmail.ocm>
2023-06-27 17:35:40 -0300

    Styled-components

---
+++ curso02/secao15/src/pages/Login/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Title } from './styled'
+import { Container } from "../../styles/GlobalStyles";
+
+export default function Login() {
+    return (
+        <Container>
+            <Title isRed={false}>
+                Login
+                <small>Faça login</small>
+            </Title>
+
+            <p>Lorem ipsum dolor sit amet.</p>
+        </Container>
+    )
+}
